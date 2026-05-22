9dd30cb63a9cdc178d545c4dd9adb2d9e888a525
Guess2007 <enzogamer70yt@gmail.com>
2026-05-21 22:11:26 -0500

    -

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indique que proceso desea ejecutar");
+            Console.WriteLine("1. Lista \n2. Array");
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Dato invalido, intente de nuevo...");
+            }
+            Console.Clear();
+            if (n == 1)
+            {
+                Lista();
+            }
+            else if (n == 2)
+            {
+                Array();
+            }
+            else
+            {
+                Console.WriteLine("Comando invalido, intenta de nuevo...");
+            }
+        }
+
+        static public void Lista()
+        {
+            Console.WriteLine("¿Que tipo de lista desea?");
+            Console.WriteLine("1. Lista numerica \n2. Lista de caracteres \n3. Lista de palabras");
+
+            int n, n1;
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Dato invalido, intente de nuevo...");
+                Console.Clear();
+            }
+            Console.WriteLine("¿Cual será el tamaño destinado?");
+            while (!int.TryParse(Console.ReadLine(), out n1))
+            {
+                Console.WriteLine("Dato invalido, intente de nuevo...");
+                Console.Clear();
+            }
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("Ejecutando proceso de lista numerica...");
+                    List<int> listaNumerica = new List<int>();
+                    listaNumerica.Capacity = n1;
+                    int reader1;
+                    Console.WriteLine("ingrese sus elementos en la siguiente linea: ");
+                    while (!int.TryParse(Console.ReadLine(), out reader1))
+                    {
+                        Console.Write("Dato invalido, intente de nuevo...");
+                    }
+                    for (int i = 0; i < n1; i++)
+                    {
+                        Console.WriteLine("elemento nº" + (i + 1) + ":"); listaNumerica.Add(reader1);
+                    }
+
+                    foreach (int i in listaNumerica)
+                    {
+                        Console.WriteLine("elemento nº" + (listaNumerica.IndexOf(i) + 1) + ": " + i);
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Ejecutando proceso de lista de caracteres...");
+                    List<char> listaCaracteres = new List<char>(n1);
+                    break;
+                case 3:
+                    Console.WriteLine("Ejecutando proceso de lista de palabras...");
+                    List<string> listaPalabras = new List<string>(n1);
+                    break;
+                default:
+                    Console.WriteLine("Comando invalido, intenta de nuevo...");
+                    break;
+            }
+
+        }
+        static public void Array()
+        {
+            Console.WriteLine("Ejecutando proceso de array...");
+        }
+    }
+}
