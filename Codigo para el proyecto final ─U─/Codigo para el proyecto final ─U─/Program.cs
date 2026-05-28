f43ffcc1488671ad1823f043286b81cca8fdce60
Guess2007 <enzogamer70yt@gmail.com>
2026-05-28 01:19:00 -0500

    .

--- Codigo para el proyecto final ─U─/Codigo para el proyecto final ─U─/Program.cs
+++ Codigo para el proyecto final ─U─/Codigo para el proyecto final ─U─/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Codigo_para_el_proyecto_final__U_
 {
@@ -10,50 +12,90 @@ namespace Codigo_para_el_proyecto_final__U_
     {
         static void Main(string[] args)
         {
-            /*
-            Console.WriteLine("Bienvenido al sistema de registro de trabajadores");
-            Console.WriteLine("Registre el numero de trabajadores: ");
-            int numeroDeUsuarios = int.Parse(Console.ReadLine());
-            string[] trabajadores = new string[numeroDeUsuarios];
-            Console.WriteLine("Registre el nombre de los trabajadores ");
-            for (int i = 0; i < numeroDeUsuarios; i++) 
-            {               
-                Console.WriteLine("Trabajador nº" + (i + 1) + ": ");
-                trabajadores[i] = Console.ReadLine();
-            }
-            Console.WriteLine("Los trabajadores registrados son: ");
-            for (int i = 0; i < numeroDeUsuarios; i++) 
+            Console.WriteLine("Bienvenido al login...");
+            Console.WriteLine("Para empezar, registre el numero de trabajadores: "); int n = int.Parse(Console.ReadLine());
+            string[,] trabajadores = new string[n + 1, 4];
+            trabajadores[0, 0] = "Nombre";
+            trabajadores[0, 2] = "contraseña";
+            trabajadores[0, 1] = "Cargo";
+            trabajadores[0, 3] = "salario";
+            for (int i = 0; i < n; i++) 
             {
-                Console.WriteLine("Trabajador nº" + (i + 1) + ": " + trabajadores[i]);
+                Console.WriteLine("Ingrese el " + trabajadores[0, 0] + " del trabajador nº " + (i + 1) + ": ");
+                trabajadores[i + 1, 0] = Console.ReadLine();
             }
-            */
-            Console.WriteLine("Bienvenido al sistema de registro de trabajadores");
-            Console.WriteLine("Ingrese el numero de trabajadores: "); int numeroDeUsuarios = int.Parse(Console.ReadLine());
-            string[,] trabajadores = new string[numeroDeUsuarios, 3];
-            Console.WriteLine("");
-            for (int i = 0; i < trabajadores.Length; i++) 
+            for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < 2; j++) 
+                Console.WriteLine("Ingrese el " + trabajadores[0, 1] + " del trabajador nº " + (i + 1) + ": ");
+                trabajadores[i + 1, 1] = Console.ReadLine();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Ingrese el " + trabajadores[0, 2] + " del trabajador nº " + (i + 1) + ": ");
+                trabajadores[i + 1, 2] = Convert.ToString(Console.ReadLine());
+            }
+            Console.WriteLine("Lista actual de registros...");
+            
+            for (int i = 0; i < n; i++) 
+            {
+                Console.WriteLine("trabajador nº " + (i + 1) + ": " + trabajadores[i + 1, 0]);
+                Console.WriteLine("Cargo: " + trabajadores[i + 1, 1]);
+                Console.WriteLine("Salario: " + trabajadores[i + 1, 2]); 
+            }
+            Console.WriteLine("Trabajadores registrados exitosamente...");
+            Console.Clear();
+            Console.WriteLine("Bienvenido al sistema de consulta de trabajadores...");
+            Console.WriteLine("Para empezar su nombre"); string nombre = Console.ReadLine();
+            Console.WriteLine("ingrese su rol"); string rol = Console.ReadLine();
+            for (int i = 0; i < n; i++) 
+            {
+                if (trabajadores[i + 1, 0] == nombre) 
                 {
-                    Console.WriteLine("ingrese el nombre del trabajador: ");
-                    trabajadores[i, 0] = Console.ReadLine();
-                    Console.WriteLine("ingrese el rol del trabajdor: ");
-                    trabajadores[i, 1] = Console.ReadLine();
-                    Console.WriteLine("ingrese el salario del trabajador: ");
-                    trabajadores[i, 2] = Console.ReadLine();
+                    Console.WriteLine("Bienvenido, " + nombre);
+                    Console.WriteLine("Cargo: " + rol);
+                    Console.WriteLine("Para proceder con el sistema, por favor, ingrese su contraseña: ");
+                    Console.WriteLine("ingrese su contraseña"); string contraseña = Console.ReadLine();
+                    if (trabajadores[i + 1, 2] == contraseña)
+                    {
+                        Console.WriteLine("Contraseña correcta, acceso concedido.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contraseña incorrecta, acceso denegado.");
+                        Environment.Exit(0);
+                    }
+                    if (rol == "administrador")
+                    {
+                        sistema_administradores(n, trabajadores);
+                    }
+                    else if (rol == "vendedor")
+                    {
+                        sistema_vendedores();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Credenciales invalidas, cerrando sistema...");
+                        Console.ReadKey();
+                    }
                 }
             }
-            Console.WriteLine("su lista: ");
-            for (int i = 0; i < trabajadores.Length; i++) 
-            {
-                for (int j = 0; j < 2; j++) 
-                {
-                    Console.WriteLine("Nombre: " + trabajadores[i, 0]);
-                    Console.WriteLine("Rol: " + trabajadores[i, 1]);
-                    Console.WriteLine("Salario: " + trabajadores[i, 2]);
-                }
-            }
-
+            
         }
+        static public void sistema_vendedores() 
+        { 
+        }
+        static public void sistema_administradores(int n, string[,] a)
+        {
+            
+            Console.WriteLine("Bienvenido al sistema de administradores...");
+            Console.WriteLine("Esta es su lista de trabajadores registrados...");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("trabajador nº " + (i + 1) + ": " + a[i + 1, 0]);
+                Console.WriteLine("Cargo: " + a[i + 1, 1]);
+                Console.WriteLine("Salario: " + a[i + 1, 2]);
+            }
+        } 
+
     }
 }
---
+++ codigo para el proyecto final 2.0/codigo para el proyecto final 2.0/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Codigo_para_el_proyecto_final__U_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Bienvenido al sistema de registro de trabajadores");
+            Console.WriteLine("Registre el numero de trabajadores: ");
+            int numeroDeUsuarios = int.Parse(Console.ReadLine());
+            string[] trabajadores = new string[numeroDeUsuarios];
+            Console.WriteLine("Registre el nombre de los trabajadores ");
+            for (int i = 0; i < numeroDeUsuarios; i++) 
+            {               
+                Console.WriteLine("Trabajador nº" + (i + 1) + ": ");
+                trabajadores[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Los trabajadores registrados son: ");
+            for (int i = 0; i < numeroDeUsuarios; i++) 
+            {
+                Console.WriteLine("Trabajador nº" + (i + 1) + ": " + trabajadores[i]);
+            }
+            */
+            /*
+            Console.WriteLine("Bienvenido al sistema de registro de trabajadores");
+            Console.WriteLine("Ingrese el numero de trabajadores: "); int numeroDeUsuarios = int.Parse(Console.ReadLine());
+            string[,] trabajadores = new string[numeroDeUsuarios, 3];
+            Console.WriteLine("");
+            for (int i = 0; i < trabajadores.Length; i++) 
+            {
+                for (int j = 0; j < 2; j++) 
+                {
+                    Console.WriteLine("ingrese el nombre del trabajador: ");
+                    trabajadores[i, 0] = Console.ReadLine();
+                    Console.WriteLine("ingrese el rol del trabajdor: ");
+                    trabajadores[i, 1] = Console.ReadLine();
+                    Console.WriteLine("ingrese el salario del trabajador: ");
+                    trabajadores[i, 2] = Console.ReadLine();
+                }
+            }
+            Console.WriteLine("su lista: ");
+            for (int i = 0; i < trabajadores.Length; i++) 
+            {
+                for (int j = 0; j < 2; j++) 
+                {
+                    Console.WriteLine("Nombre: " + trabajadores[i, 0]);
+                    Console.WriteLine("Rol: " + trabajadores[i, 1]);
+                    Console.WriteLine("Salario: " + trabajadores[i, 2]);
+                }
+            }
+            */
+            
+            string connectionString = "Data Source=localhost;Initial Catalog=TrabajadoresDB;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Bienvenido al sistema de registro de trabajadores");
+                Console.WriteLine("Ingrese el numero de trabajadores: "); int numeroDeUsuarios = int.Parse(Console.ReadLine());
+                string nombre, rol;
+                decimal salario;
+                for (int i = 0; i < numeroDeUsuarios; i++)
+                {
+                    Console.WriteLine("ingrese el nombre del trabajador: ");
+                    nombre = Console.ReadLine();
+                    Console.WriteLine("ingrese el rol del trabajador: ");
+                    rol = Console.ReadLine();
+                    Console.WriteLine("ingrese el salario del trabajador: ");
+                    salario = decimal.Parse(Console.ReadLine());
+                }
+                connection.Open();
+                Console.WriteLine("Conexión a la base de datos establecida.");
+                connection.Insert("Insert into Trabajadores (Nombre, Rol, Salario) VALUES ('" + nombre + "', '" + rol + "', " + salario + ")");
+                connection.Select("SELECT * FROM Trabajadores");
+                // Aquí puedes agregar código para interactuar con la base de datos, como insertar, actualizar o consultar datos.
+                connection.Close();
+                Console.WriteLine("Conexión a la base de datos cerrada.");
+            }
+        }
+    }
