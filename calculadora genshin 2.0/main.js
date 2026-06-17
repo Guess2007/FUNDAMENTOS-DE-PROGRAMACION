9b1c53dcf8fc617e06c463ea4d1a9ec54b2d3267
Guess2007 <enzogamer70yt@gmail.com>
2026-06-16 19:57:59 -0500

    .

--- calculadora genshin 2.0/main.js
+++ calculadora genshin 2.0/main.js
@@ -1,6 +1,18 @@
-const navbar = document.getElementById("navbar")
-function cambiarpagina(){
-    addEventListener('click', () => {
-    
-    });
+fetch("navbar.html")
+      .then(res => res.text())
+      .then(html => {
+        document.getElementById("navbar-container").innerHTML = html;
+
+        // Resalta el enlace activo según la página actual
+        const links = document.querySelectorAll(".nav-links a");
+        links.forEach(link => {
+          if (link.href === window.location.href) {
+            link.classList.add("active");
+          }
+        });
+      });
+ 
+function toggleSeccion(){
+    const seccion = Document.getElementById("Calculadora_2");
+    seccion.classList.toggleSeccion("visible")
 }
