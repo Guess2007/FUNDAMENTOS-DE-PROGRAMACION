58e5d39477c7a7882f5329284323be37868590f8
Guess2007 <enzogamer70yt@gmail.com>
2026-07-01 22:31:14 -0500

    a

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Persona> personas = new List<Persona>()
+{
+    new Persona { Nombre = "Ana", Edad = 25 },
+    new Persona { Nombre = "Luis", Edad = 40 },
+    new Persona { Nombre = "Juan", Edad = 30 }
+};
+
+            // Ordenar de mayor a menor por la propiedad 'Edad'
+            var personasOrdenadas = personas.OrderByDescending(p => p.Edad).ToList();
+            for (int i = 0; i < personasOrdenadas.Count; i++)
+            {
+                Console.WriteLine($"Nombre: {personasOrdenadas[i].Nombre}, Edad: {personasOrdenadas[i].Edad}");
+            }
+        }
+    }
+}
